f626a23a410e32e58791dd331ef0e3ffbf982c05
Jonas Kalivoda <jonaskalivoda@seznam.cz>
2023-02-19 22:49:51 +0100

    Added login & register

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="formWrapper">
         <h2>Login</h2>
-        <form action="">
+        <form @submit.prevent="submitForm()">
             <label for="email" class="form-label">Email: </label>
-            <input type="email" class="form-control" id="email">
+            <input v-model="email" type="email" :class="{ 'form-control': true, 'is-invalid': !emailIsValid }" id="email">
 
             <label for="password" class="form-label">Password: </label>
-            <input type="password" class="form-control" id="password">
+            <input v-model="password" type="password" :class="{ 'form-control': true, 'is-invalid': !passwordIsValid }" id="password" >
 
             <div class="center">
                 <input type="submit" value="Log in" class="btn btn-primary submitButton">
@@ -14,6 +14,36 @@
         </form>
     </div>
 </template>
+
+<script>
+import * as LoginViewModel from '../viewmodels/loginViewModel.js'
+
+export default {
+    data() {
+        return {
+            email: '',
+            password: '',
+            emailIsValid: true,
+            passwordIsValid: true,
+        }
+    },
+    methods: {
+        submitForm() {
+            if (LoginViewModel.validateEmail(this.email)) {
+                this.emailIsValid = true;
+                if (LoginViewModel.validatePassword(this.password)) {
+                    LoginViewModel.login(this.email, this.password);
+                }
+                else 
+                this.passwordIsValid = false;
+            }
+            else {
+                this.emailIsValid = false;
+            }
+        },
+    },
+}
+</script>
 
 <style scoped>
 @import '../assets/forms.css';
